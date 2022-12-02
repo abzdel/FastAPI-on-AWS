dba94dbc2275fe6dfaae86ad8c0f584a3afd5301
abzdel <abzdel25@gmail.com>
2022-12-02 18:30:48 +0000

    api framework built, working on passing sample data through

--- mlapi.py
+++ mlapi.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pickle
+import pandas as pd
 
 app = FastAPI()
 
@@ -8,8 +10,32 @@ class Item(BaseModel):
     stl_5:float
     ft_pct_5: float
     min_5: float
+    
+with open("model/ppg_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
 
 
 @app.get("/")
 async def scoring_endpoint(item:Item):
-    return item
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+    y_het = model.predict(df)
+    return {"prediction": int(y_hat)}
+    
+@app.get("/predict/{filename}")
+async def predict_endpoint(filename):
+    df = pd.DataFrame([filename.dict().values()], columns=filename.dict().keys())
+    return {"original data": df}
+    #y_het = model.predict(df)
+    #return {"prediction": int(y_hat)}
+    
+    
+@app.get("/test")
+async def test_hello():
+    return {"message": "hello"}
+"""
+@app.get("/test/")
+async def test(file):
+    df = pd.read_csv("sample.csv",index_col=False)
+    return df
+"""
