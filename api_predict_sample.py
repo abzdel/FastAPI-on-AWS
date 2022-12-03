ed839c076ceff6209dda5cdfb6b9139421d2fd8d
Alex Bzdel <ab913@duke.edu>
2022-12-03 20:00:41 +0000

    finished api - now working. should deploy to aws soon

--- api_predict_sample.py
+++ api_predict_sample.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 data_json = {
-    "PTS_5": 16.5,
+    "pts_5": 16.5,
     "stl_5": 0.3,
     "ft_pct_5": 87.2,
     "min_5": 41.7
@@ -20,7 +20,7 @@ print(f"here is our output: {r}")
 
 
 X = [16.5, 0.3, 87.2, 41.7]
-features = ["PTS_5", "stl_5", "ft_pct_5", "min_5"]
+features = ["pts_5", "stl_5", "ft_pct_5", "min_5"]
 
 url_string = host + f'/predict?{features[0]}={X[0]}&{features[1]}={X[1]}&{features[2]}={X[2]}&{features[3]}={X[3]}'
 r = requests.get(url_string)
--- mlapi.py
+++ mlapi.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
-#import pandas as pd
+import pandas as pd
 import uvicorn
+import numpy as np
 
 app = FastAPI()
 
-class Stats(BaseModel):
-    PTS_5: float
-    stl_5:float
-    ft_pct_5: float
-    min_5: float
-    
-    
+
+# class Stats(BaseModel):
+#     PTS_5: float
+#     stl_5: float
+#     ft_pct_5: float
+#     min_5: float
+
 
 #df = pd.read_csv("sample.csv")
 #df.drop(["Unnamed: 4"], axis=1, inplace=True)
 
-    
+
 with open("model/ppg_model.pkl", "rb") as f:
     model = pickle.load(f)
 
@@ -27,29 +28,13 @@ async def root():
     return {"hello": "mlapi"}
 
 
-  
-@app.get("/predict")
-async def predict_ppg(data:Stats):
-    
-    data = data.dict()
-    PTS_5 = data["PTS_5"]
-    stl_5 = data["stl_5"]
-    ft_pct_5 = data["ft_pct_5"]
-    min_5 = data["min_5"]
-    
-    pred = model.predict([[PTS_5, stl_5, ft_pct_5, min_5]])
-    return {"prediction": pred}
+@app.get("/predict/{pts_5}/{stl_5}/{ft_pct_5}/{min_5}")
+async def predict_ppg(pts_5, stl_5, ft_pct_5, min_5):
 
-@app.post("/test")
-async def test_post(data:Stats):
-        
-    pred = model.predict(data)
-    return {"prediction": pred}
+    pred = model.predict(np.array([pts_5, stl_5, ft_pct_5, min_5]).reshape(1,-1))
+    return {"prediction": pred[0]}
 
-    
-    
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="127.0.0.1", port=8000)
+
 
 if __name__ == '__main__':
     uvicorn.run(app, port=8080, host='0.0.0.0')
